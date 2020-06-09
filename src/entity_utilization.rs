59dd0c960351b6f7da3b7a8928cf15f5269bbffc
fjaelltrollet <fjaelltrollet@users.noreply.github.com>
2020-06-09 14:46:51 +0200

    Make EntityUtilization computable and comparable

--- src/entity_utilization.rs
+++ src/entity_utilization.rs
@@ -1,8 +1,52 @@
-#[derive(Debug)]
+use std::ops;
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
 pub struct EntityUtilization {
-    lut: u32,
-    reg: u32,
-    dsp: u32,
-    bram18: u32,
-    uram: u32,
+    pub lut: i32,
+    pub reg: i32,
+    pub dsp: i32,
+    pub bram18: i32,
+    pub uram: i32,
+}
+
+impl<'a> ops::Neg for &'a EntityUtilization {
+    type Output = EntityUtilization;
+
+    fn neg(self) -> EntityUtilization {
+        EntityUtilization {
+            lut: -self.lut,
+            reg: -self.reg,
+            dsp: -self.dsp,
+            bram18: -self.bram18,
+            uram: -self.uram
+        }
+    }
+}
+
+impl<'a, 'b> ops::Add<&'b EntityUtilization> for &'a EntityUtilization {
+    type Output = EntityUtilization;
+
+    fn add(self, other: &'b EntityUtilization) -> EntityUtilization {
+        EntityUtilization {
+            lut: self.lut + other.lut,
+            reg: self.reg + other.reg,
+            dsp: self.dsp + other.dsp,
+            bram18: self.bram18 + other.bram18,
+            uram: self.uram + other.uram,
+        }
+    }
+}
+
+impl<'a, 'b> ops::Sub<&'b EntityUtilization> for &'a EntityUtilization {
+    type Output = EntityUtilization;
+
+    fn sub(self, other: &'b EntityUtilization) -> EntityUtilization {
+        EntityUtilization {
+            lut: self.lut - other.lut,
+            reg: self.reg - other.reg,
+            dsp: self.dsp - other.dsp,
+            bram18: self.bram18 - other.bram18,
+            uram: self.uram - other.uram,
+        }
+    }
 }
